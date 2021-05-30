2241a135fdf1441a25e22f555fcfeb4787c07ac6
Evgene <mrevgenix@gmail.com>
2021-05-30 14:19:49 +0700

    Use logging middleware

--- wine_log/bot/__main__.py
+++ wine_log/bot/__main__.py
@@ -12,6 +12,7 @@ from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types import ParseMode
 from aiogram.utils import executor
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from wine_log.db import OrmSession
 from wine_log.db.models import User, TastingRecord, WinePhoto
 
@@ -67,11 +68,7 @@ async def cmd_start(message: types.Message):
 @dp.message_handler(chat_type=types.ChatType.PRIVATE, content_types=types.ContentTypes.PHOTO, state=Form.photo)
 async def process_photo(message: types.Message, state: FSMContext):
     dt = message.date
-    current_state = await state.get_state()
-    log.info('process_photo %s', current_state)
-
     largest_index = message.photo.index(max(message.photo, key=lambda photo: photo.width))
-
     await state.update_data(
         photo_file_id=message.photo[largest_index].file_id,
         s3_obj_key=f'{dt.year}{str(dt.month).rjust(2, "0")}{str(dt.day).rjust(2, "0")}_'
@@ -84,8 +81,6 @@ async def process_photo(message: types.Message, state: FSMContext):
 
 @dp.message_handler(chat_type=types.ChatType.PRIVATE, content_types=types.ContentTypes.TEXT, state=Form.wine_name)
 async def process_wine_name(message: types.Message, state: FSMContext):
-    current_state = await state.get_state()
-    log.info('process_wine_name %s', current_state)
     await state.update_data(wine_name=message.text)
     await Form.next()
     await message.reply("Какой у него регион происхождения?")
@@ -93,8 +88,6 @@ async def process_wine_name(message: types.Message, state: FSMContext):
 
 @dp.message_handler(chat_type=types.ChatType.PRIVATE, content_types=types.ContentTypes.TEXT, state=Form.region)
 async def process_region(message: types.Message, state: FSMContext):
-    current_state = await state.get_state()
-    log.info('process_region %s', current_state)
     await state.update_data(region=message.text)
     await Form.next()
     await message.reply("Из каких сортов винограда оно сделано?")
@@ -102,8 +95,6 @@ async def process_region(message: types.Message, state: FSMContext):
 
 @dp.message_handler(chat_type=types.ChatType.PRIVATE, content_types=types.ContentTypes.TEXT, state=Form.grapes)
 async def process_grapes(message: types.Message, state: FSMContext):
-    current_state = await state.get_state()
-    log.info('process_grapes %s', current_state)
     await state.update_data(grapes=message.text)
     await Form.next()
     await message.reply("Наконец, какие ваши ощущения? Пишите свободно.")
@@ -113,18 +104,16 @@ async def process_grapes(message: types.Message, state: FSMContext):
 async def process_experience(message: types.Message, state: FSMContext):
     sender = message.from_user
     dt = message.date
-    current_state = await state.get_state()
-    log.info('process_experience %s', current_state)
     async with state.proxy() as data:
         data['experience'] = message.text
 
+        log.info(f'Getting file {data["photo_file_id"]} and downloading it to memory buffer')
         file = await bot.get_file(data['photo_file_id'])
         file_buf = io.BytesIO()
         await file.download(file_buf)
 
-        log.info('s3 put_object: submitting to pool')
+        log.info(f'Put file {data["photo_file_id"]} to s3 with key {data["s3_obj_key"]}')
         future = pool.submit(s3bucket.put_object, Key=data['s3_obj_key'], Body=file_buf)
-        log.info('s3 put_object: awaiting wrapped future')
         await asyncio.wrap_future(future)
 
         async with OrmSession() as session:
@@ -152,4 +141,5 @@ async def process_experience(message: types.Message, state: FSMContext):
 
 
 if __name__ == '__main__':
+    dp.middleware.setup(LoggingMiddleware(log))
     executor.start_polling(dp, skip_updates=True)
